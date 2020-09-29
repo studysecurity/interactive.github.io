c49a15b14ee204b1986025f08c9fe8fe52a9a169
ssp5746 <ssp@gmail.com>
2020-09-29 17:37:26 +0900

    최종 완성본 (스트로크 추가)

--- js/main.js
+++ js/main.js
@@ -112,9 +112,40 @@
         blendHeight: [0, 0, { start: 0, end: 0 }],
         canvas_scale: [0, 0, { start: 0, end: 0 }],
         canvasCaption_opacity: [0, 1, { start: 0, end: 0 }],
-        canvasCaption_translateY: [20, 0, { start: 0, end: 0 }],
+        canvasCaption_translateY: [40, 0, { start: 0, end: 0 }],
         rectStartY: 0
       }
+    },
+    {
+      type: 'sticky',
+      heightNum: 5.2,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-4"),
+        pencilLogo: document.querySelector("#scroll-section-4 .pencil-logo"),
+        pencil: document.querySelector("#scroll-section-4 .pencil"),
+        ribbonPath: document.querySelector(".ribbon-path path"),
+        messageA: document.querySelector("#scroll-section-4 .main-message.a"),
+        messageB: document.querySelector("#scroll-section-4 .main-message.b"),
+      },
+      values: {
+        messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+        messageB_opacity_in: [0, 1, { start: 0.4, end: 0.5 }],
+        messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
+        messageA_opacity_out: [1, 0, { start: 0.3, end: 0.4 }],
+        messageB_opacity_out: [1, 0, { start: 0.6, end: 0.7 }],
+        messageA_translateY_out: [0, -20, { start: 0.3, end: 0.4 }],
+        pencilLogo_width_in: [1000, 200, { start: 0.1, end: 0.4 }],
+        pencilLogo_width_out: [200, 50, { start: 0.4, end: 0.8 }],
+        pencilLogo_translateX_in: [-10, -20, { start: 0.2, end: 0.4 }],
+        pencilLogo_translateX_out: [-20, -50, { start: 0.4, end: 0.8 }],
+        pencilLogo_opacity_out: [1, 0, { start: 0.8, end: 0.9 }],
+        pencil_right: [-10, 70, { start: 0.3, end: 0.8 }],
+        pencil_bottom: [-80, 100, { start: 0.3, end: 0.8 }],
+        pencil_rotate: [-120, -200, { start: 0.3, end: 0.8 }],
+        path_dashoffset_in: [1401, 0, { start: 0.2, end: 0.4 }],
+        path_dashoffset_out: [0, -1401, { start: 0.6, end: 0.8 }],
+      },
     }
   ];
 
@@ -214,6 +245,7 @@
     const scrollHeight = sceneInfo[currentScene].scrollHeight;
     const scrollRatio = currentYOffset / scrollHeight;
 
+    console.log(currentScene);
     switch (currentScene) {
       case 0:
         objs.canvas.style.opacity = calcValues(values.canvas_opacity, currentYOffset);
@@ -431,6 +463,97 @@
             objs.canvasCaption.style.transform = `translate3d(0, ${calcValues(values.canvasCaption_translateY, currentYOffset)}%, 0)`;
           }
         }
+        break;
+
+      case 4:
+        if (scrollRatio <= 0.25) {
+          // in
+          objs.messageA.style.opacity = calcValues(
+            values.messageA_opacity_in,
+            currentYOffset
+          );
+          objs.messageA.style.transform = `translate3d(0, ${calcValues(
+            values.messageA_translateY_in,
+            currentYOffset
+          )}%, 0)`;
+        } else {
+          // out
+          objs.messageA.style.opacity = calcValues(
+            values.messageA_opacity_out,
+            currentYOffset
+          );
+          objs.messageA.style.transform = `translate3d(0, ${calcValues(
+            values.messageA_translateY_out,
+            currentYOffset
+          )}%, 0)`;
+        }
+
+        if (scrollRatio <= 0.55) {
+          // in
+          objs.messageB.style.opacity = calcValues(
+            values.messageB_opacity_in,
+            currentYOffset
+          );
+        } else {
+          // out
+          objs.messageB.style.opacity = calcValues(
+            values.messageB_opacity_out,
+            currentYOffset
+          );
+        }
+
+        // 크기가 커져도 깨지지 않는 SVG의 장점을 살리기 위해 transform scale 대신 width를 조정
+        if (scrollRatio <= 0.4) {
+          objs.pencilLogo.style.width = `${calcValues(
+            values.pencilLogo_width_in,
+            currentYOffset
+          )}vw`;
+          objs.pencilLogo.style.transform = `translate(${calcValues(
+            values.pencilLogo_translateX_in,
+            currentYOffset
+          )}%, -50%)`;
+        } else {
+          objs.pencilLogo.style.width = `${calcValues(
+            values.pencilLogo_width_out,
+            currentYOffset
+          )}vw`;
+          objs.pencilLogo.style.transform = `translate(${calcValues(
+            values.pencilLogo_translateX_out,
+            currentYOffset
+          )}%, -50%)`;
+        }
+
+        // 빨간 리본 패스(줄 긋기)
+        if (scrollRatio <= 0.5) {
+          objs.ribbonPath.style.strokeDashoffset = calcValues(
+            values.path_dashoffset_in,
+            currentYOffset
+          );
+        } else {
+          objs.ribbonPath.style.strokeDashoffset = calcValues(
+            values.path_dashoffset_out,
+            currentYOffset
+          );
+        }
+
+        objs.pencilLogo.style.opacity = calcValues(
+          values.pencilLogo_opacity_out,
+          currentYOffset
+        );
+        objs.pencil.style.right = `${calcValues(
+          values.pencil_right,
+          currentYOffset
+        )}%`;
+        objs.pencil.style.bottom = `${calcValues(
+          values.pencil_bottom,
+          currentYOffset
+        )}%`;
+        objs.pencil.style.transform = `rotate(${calcValues(
+          values.pencil_rotate,
+          currentYOffset
+        )}deg)`;
+
+
         break;
     }
   }
